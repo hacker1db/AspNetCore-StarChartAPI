84256de9b6cbd558c706bb436da8cce5ef0349a2
hacker1db <hacker1db@outlook.com>
2020-12-16 15:20:44 -0800

    routes have been added.

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        // GET
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return new EmptyResult();
+        }
+
+        public CelestialObjectController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+    }
+}
